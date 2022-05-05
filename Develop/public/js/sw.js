2889dfc43e556e9f9ebf5300658fcb6806b97ba1
Alec Harvey <ruinhard@gmail.com>
2022-05-04 19:50:50 -0500

    added manifest

--- Develop/public/js/sw.js
+++ Develop/public/js/sw.js
@@ -1,5 +1,5 @@
 if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("./js/sw-cacher.js")
+    navigator.serviceWorker.register("../sw-cacher.js")
     .then(()=>console.log("Service worker registered"))
     .catch((err)=>console.log("failed to register service worker: ",err))
 }
--- Develop/public/sw-cacher.js
+++ Develop/public/sw-cacher.js
@@ -4,16 +4,19 @@ const files_to_cache=[
 const app_prefix= "PWA_Challenge-"
 const version = "v1"
 const Cache_Name=app_prefix+version
+
 self.addEventListener("install",(event)=>{
     event.waitUntil(
         caches.open(Cache_Name)
         .then((cache)=>{
             console.log("installing cache");
-            return cache.addAll(files_to_cache)
+            return cache.addAll(files_to_cache)//create cache when installed and save all files to cache
         })
     )
 })
-self.addEventListener('activate', function(e) {
+
+self.addEventListener('activate', function(e) {//delete old caches
+  console.log("service worker activated");
     e.waitUntil(
       caches.keys().then(function(keyList) {
         let cacheKeeplist = keyList.filter(function(key) {
@@ -31,16 +34,18 @@ self.addEventListener('activate', function(e) {
       })
     );
   });
+
 self.addEventListener("fetch",(event)=>{
     console.log("fetch request: ",event.request.url);
+    console.log("caches: ",caches);
     event.respondWith(
         caches.match(event.request)
         .then((request)=>{
             if (request){
-                console.log("responding with cache: ",event.request.url);
+                console.log("responding with cache: ",event.request.url);//respond with cache
                 return request
             }else{
-                console.log("file is not cached, fetching : ",event.request.url);
+                console.log("file is not cached, fetching : ",event.request.url);//respond with request 
                 return fetch(event.request)
             }
         })
